f9dd434e07770a788b430a9cb665ab509605cf4b
D <dhy@jshxlsp.com>
2023-01-10 14:02:21 +0800

    fix: 修改基础信息

--- src/hooks/useAnimatedComponents.ts
+++ src/hooks/useAnimatedComponents.ts
@@ -1,11 +1,3 @@
-/**
- * Copyright (c) JOB TODAY S.A. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-
 import { Animated } from "react-native";
 
 const INITIAL_POSITION = { x: 0, y: 0 };
